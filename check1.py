4047a2268378102f63fa08c1dd6175f9e8838ef1
srijakodakandla <67781841+srijakodakandla@users.noreply.github.com>
2024-04-29 16:43:04 -0400

    Update check1.py

--- check1.py
+++ check1.py
@@ -1,8 +1,44 @@
+import psycopg2
+from psycopg2 import extras
 import streamlit as st
-import pandas as pd
- 
+
 st.write("""
 # My first app
-jai sree ram
+jai shree ram
 """)
- 
+
+
+def connect_to_database():
+    return psycopg2.connect(
+        dbname="hsptl_managment",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="8501"
+    )
+
+def execute_query(query):
+    connection = connect_to_database()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+def main():
+    st.title("PostgreSQL Query Visualizer")
+
+    # Input SQL query
+    query = st.text_area("Enter SQL Query")
+
+    if st.button("Execute Query"):
+        # Execute the query and display results
+        result = execute_query(query)
+        if result:
+            st.write("Query Results:")
+            st.write(result)
+        else:
+            st.write("No results to display.")
+
+if __name__ == "__main__":
+    main()
